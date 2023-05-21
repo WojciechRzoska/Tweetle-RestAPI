6f5979dec656c90642f10d9bc87c993704d8b89f
WojciechRzoska <wojciechrzoska@gmail.com>
2023-05-21 23:00:07 +0200

    add models with basing functions

---
+++ app/Models/Follow.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Follow extends Model
+{
+    use HasFactory;
+
+    public function follow(): BelongsTo
+    {
+        return $this->belongs(User::class, 'follower_id');
+    }
+
+    public function followed(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'follower_id');
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use App\Models\Post;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -42,4 +46,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function userProfile() : HasOne
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
 }
